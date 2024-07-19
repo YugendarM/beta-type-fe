6fc5d20cf678ec3f35e8df241db8f2dcf664ef28
YugendarM <yugendar2411@gmail.com>
2024-07-19 11:34:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <p className='text-2xl text-blue-100 bg-blue-500'>App</p>
+    </React.Fragment>
+  )
+}
+
+export default App
